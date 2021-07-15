abbe080f4ff7818881d939d1f06f8becd95cc46e
scilla <scilla@student.42.fr>
2021-07-15 17:15:03 +0200

    up

--- philo/generator.c
+++ philo/generator.c
@@ -17,24 +17,30 @@ t_phil	*gen_phil(t_opt *opt, int i)
 	return (phil);
 }
 
+void	wrong_args(t_opt *opt)
+{
+	free(opt);
+	printf("wrong args\n");
+	exit(0);
+}
+
 t_opt	*gen_opt(int argc, char **argv)
 {
 	t_opt	*opt;
 
-	if (argc < 5 || argc > 6)
-	{
-		printf("wrong args\n");
-		exit(0);
-	}
 	opt = malloc(sizeof(t_opt));
+	if (argc < 5 || argc > 6)
+		wrong_args(opt);
 	opt->p_count = ft_atoi(argv[1]);
 	opt->time_die = ft_atoi(argv[2]);
 	opt->time_eat = ft_atoi(argv[3]);
 	opt->time_sleep = ft_atoi(argv[4]);
+	opt->eat_count = -1;
 	if (argc == 6)
 		opt->eat_count = ft_atoi(argv[5]);
-	else
-		opt->eat_count = -1;
+	if (!opt->p_count || !opt->time_die || !opt->time_eat || !opt->time_sleep
+		|| !opt->eat_count)
+		wrong_args(opt);
 	opt->alive = 1;
 	opt->phils = malloc(sizeof(t_phil *) * opt->p_count);
 	init_mutex(opt);
--- philo_bonus/generator_bonus.c
+++ philo_bonus/generator_bonus.c
@@ -14,23 +14,30 @@ void	gen_sem(t_opt *opt)
 	free(sem_name);
 }
 
+void	wrong_args(t_opt *opt)
+{
+	free(opt);
+	printf("wrong args\n");
+	exit(0);
+}
+
 t_opt	*gen_opt(int argc, char **argv)
 {
 	t_opt	*opt;
 
-	if (argc < 5 || argc > 6)
-	{
-		printf("wrong args\n");
-		exit(0);
-	}
 	opt = malloc(sizeof(t_opt));
-	opt->p_count = atoi(argv[1]);
-	opt->time_die = atoi(argv[2]);
-	opt->time_eat = atoi(argv[3]);
-	opt->time_sleep = atoi(argv[4]);
+	if (argc < 5 || argc > 6)
+		wrong_args(opt);
+	opt->p_count = ft_atoi(argv[1]);
+	opt->time_die = ft_atoi(argv[2]);
+	opt->time_eat = ft_atoi(argv[3]);
+	opt->time_sleep = ft_atoi(argv[4]);
 	opt->eat_count = -1;
 	if (argc == 6)
-		opt->eat_count = atoi(argv[5]);
+		opt->eat_count = ft_atoi(argv[5]);
+	if (!opt->p_count || !opt->time_die || !opt->time_eat || !opt->time_sleep
+		|| !opt->eat_count)
+		wrong_args(opt);
 	opt->sim_start = mtime();
 	opt->phils = malloc(sizeof(t_phil *) * opt->p_count);
 	opt->pids = malloc(sizeof(int) * opt->p_count);
